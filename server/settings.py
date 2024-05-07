eaefa51506265bb5915d98652aa1339f946ee69a
martimiranda <martimiranda2356@gmail.com>
2024-05-07 18:25:53 +0200

    dhdn

--- server/settings.py
+++ server/settings.py
@@ -64,8 +64,8 @@ ROOT_URLCONF = 'server.urls'
 
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 TEMPLATES = [
     {
