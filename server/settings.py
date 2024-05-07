24a9943508e7446948a5c53563b57ec77a2c7f37
martimiranda <martimiranda2356@gmail.com>
2024-05-07 17:55:35 +0200

    dnd

--- server/settings.py
+++ server/settings.py
@@ -61,6 +61,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 #Fi
 ROOT_URLCONF = 'server.urls'
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
